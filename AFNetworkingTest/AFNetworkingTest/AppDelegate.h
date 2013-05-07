1ef6a8628eb33cbdcc8ab3d1b4234ef4529c2127
Long Vinh Nguyen <long.omni@outlook.com>
2013-05-07 09:34:13 +0700

    AFNetworking test

---
+++ AFNetworkingTest/AFNetworkingTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AFNetworkingTest
+//
+//  Created by VisiKard MacBook Pro on 5/2/13.
+//  Copyright (c) 2013 VisiKard MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
