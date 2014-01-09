bf392f16a78a80276bfdb9c56f4e91dd7c172ae3
Long Vinh Nguyen <long.omni@outlook.com>
2014-01-09 16:56:02 +0700

    Simple Weather App

---
+++ SimpleWeather/SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by VisiKard MacBook Pro on 1/9/14.
+//  Copyright (c) 2014 VLong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
