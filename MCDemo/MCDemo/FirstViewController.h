2dc99306fe825e1301a7540fa0c8797692389b08
Long Vinh Nguyen <long.omni@gmail.com>
2014-05-07 20:43:11 +0700

    MCDemo Chat

---
+++ MCDemo/MCDemo/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  MCDemo
+//
+//  Created by Long Vinh Nguyen on 5/7/14.
+//  Copyright (c) 2014 Home Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ MCDemo/MCDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MCDemo
+//
+//  Created by Long Vinh Nguyen on 5/7/14.
+//  Copyright (c) 2014 Home Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
